da41a8002d57ca6b8791f3ec3edf58566b3f53c9
poupsipoups <pauline.gobe16@gmail.com>
2023-04-18 18:53:50 +0200

    reorder elements

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,18 +1,26 @@
 <template>
-       <nav>
-    <div class="gauche">
-      <img alt="Vue logo" src="https://cdn-icons-png.flaticon.com/512/5722/5722057.png" height="40">
-      <p><strong>Poups</strong>' corner</p>
-   </div>
-   <div class="onglets">
-        <router-link to="/anime" class="link">Anime List</router-link>
-        <router-link to="/manga" class="link">Manga List</router-link>
-    </div>
-    <div class="droite" id="endNav">
-      <p>Mon profil</p>
-      <a href="..\assets\icons\hamburger.png" class="icon" @click="displayMenu"/>
-    </div>
-  </nav>
+  <header>
+      <nav>
+        <div class="gauche">
+          <img alt="Vue logo" src="https://cdn-icons-png.flaticon.com/512/5722/5722057.png" height="40">
+          <a href="#"><strong>Poups</strong>' corner</a>
+        </div>
+        <ul class="links">
+            <li><router-link to="/anime" class="link">Anime List</router-link></li>
+            <li><router-link to="/manga" class="link">Manga List</router-link></li>
+        </ul>
+
+        <a href="#" class="action_btn">My favorites</a>
+        <div class="toggle_btn">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+
+        <div class="dropdown-nav">
+          
+        </div>
+     
+      </nav>
+  </header>
 </template>
 
 <script>
@@ -37,16 +45,95 @@ export default{
 
 
 <style>
+
+li {
+  list-style: none;
+}
+
+a{
+  text-decoration: none;
+  color: #fff;
+  font-size: 1rem;
+}
+
+a:hover{
+  color: blue;
+}
+
+header{
+  position: relative;
+  padding: 0 2rem;
+}
+
 nav{
+  width: 100%;
+  height: 60px;
+  max-width: 1200px;
+  margin:0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+nav .gauche a{
+  font-size: 1.5rem;
+}
+
+nav .links{
+  display: flex;
+  gap: 2rem;
+}
+
+nav .toggle_btn{
+  color: #fff;
+  font-size : 1.5rem;
+  cursor :pointer;
+  display: none;
+}
+
+.action_btn{
+  background-color: blue;
+  color : #fff;
+  padding: 0.5rem 1rem;
+  border : none;
+  outline: none;
+  border-radius: 20px;
+  font-size: 0.8rem;
+  font-weight: bold;
+  cursor: pointer;
+  transition: scale 0.2 ease;
+}
+
+.action_btn:hover{
+  scale: 1.05;
+  color:#fff;
+}
+
+.action_btn:active{
+  scale: 0.95;
+}
+
+@media(max-width : 800px){
+  nav .links, 
+  nav .action_btn{
+    display: none;
+  }
+
+  nav .toggle_btn{
+    display: block;
+  }
+}
+
+
+
+/* nav{
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
   background-color: #EEE1D9ff;
-  overflow: hidden;
   color: white;
-  padding: 0 30px;
-  margin: 8px;
+  padding: 10px;
   border-radius: 20px;
   font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
 }
@@ -63,6 +150,8 @@ nav .gauche img{
 }
 
 nav .onglets .link{
+  display: flex;
+  flex-direction: row;
   margin-left: 10px;
   margin-left: 20px;
   color: pink;
@@ -72,6 +161,21 @@ nav .onglets .link{
 nav .onglets .link:hover{
   color: white;
 }
+
+@media screen and (max-width: 760px){
+  nav{
+    flex-direction: column;
+  }
+
+  nav .onglets{
+    flex-direction: column;
+    align-items: center;
+  }
+
+  nav .link {
+    margin : 10px 0;
+  }
+} */
 
 
 </style>
